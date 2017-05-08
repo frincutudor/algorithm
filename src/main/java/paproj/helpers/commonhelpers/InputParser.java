1e9c6a3418bfecbe52bc51a7ae815a84e746f73c
Frincu Tudor <frincu_tudor@yahoo.com>
2017-05-09 00:16:08 +0300

    Implemented endpoint and jsp

--- src/main/java/paproj/helpers/commonhelpers/InputParser.java
+++ src/main/java/paproj/helpers/commonhelpers/InputParser.java
@@ -1,5 +1,7 @@
 
 package paproj.helpers.commonhelpers;
+import paproj.algorithms.Graphs.Helpers.Edge;
+
 import java.util.ArrayList;
 
 /**
@@ -8,7 +10,7 @@ import java.util.ArrayList;
 public class InputParser {
 
 
-
+    //insertion sort
     public static ArrayList<Integer> inputParser(String stringToParse)
     {
         String strArray[] = new String[stringToParse.length()/2];
@@ -21,6 +23,19 @@ public class InputParser {
             algorithmValues.add(Integer.parseInt(strArray[i]));
 
         return algorithmValues;
+    }
+
+    public static KruskalObject kruskalInputParser(String[] input)
+    {
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+        int numberOfNodes=Integer.valueOf(input[0]);
+
+        for(int i=1;i<input.length;i++)
+        {
+            String[] splitString=input[i].split(" ");
+            edges.add(new Edge(Integer.valueOf(splitString[0]),Integer.valueOf(splitString[1]),Double.valueOf(splitString[2])));
+        }
+        return new KruskalObject(edges,numberOfNodes);
 
     }
 }
