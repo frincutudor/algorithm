2906fe05d4ae2c0efb8c9b30be18174e14b8e74b
Joywalker <alexandrurazvant525@gmail.com>
2017-05-19 13:14:17 +0300

    Added QuickHull Point

--- src/main/java/paproj/algorithms/codebase/convexhull/ConvexHull.java
+++ src/main/java/paproj/algorithms/codebase/convexhull/ConvexHull.java
@@ -1,9 +1,20 @@
 package paproj.algorithms.codebase.convexhull;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Joywalker on 5/15/2017.
  */
 public class ConvexHull {
-
     //TODO : implement convex hull
+
+    public static Set<Point> QuickHull(ArrayList<Point> points) {
+
+        //TODO: continue
+
+        return new HashSet<>();
+    }
 }
